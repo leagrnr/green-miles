f3054c24b631b6aa52d164400d694d7bfc7a6918
ElRacoon <lea.granier@outlook.fr>
2024-12-04 10:36:29 +0100

    count player

--- script/script.js
+++ script/script.js
@@ -1,29 +1,29 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Clear localStorage on page load
     localStorage.clear();
 
     const playerInputs = document.querySelectorAll('#form-player input[type="text"]');
     const form = document.getElementById('form-player');
-    const resetButton = document.getElementById('reset');
 
-    // Clear input values on page load
     playerInputs.forEach(input => {
         input.value = '';
     });
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
+
+        let playerCount = 0;
         playerInputs.forEach(input => {
+            if (input.value.trim() !== '') {
+                playerCount++;
+            }
+        });
+         if (playerCount < 2) {
+            alert('Please enter at least two players to start the game.');
+            return;
+        }
+         playerInputs.forEach(input => {
             localStorage.setItem(input.id, input.value);
         });
         window.location.href = './choice.html';
-    });
-
-    resetButton.addEventListener('click', (event) => {
-        localStorage.clear();
-        playerInputs.forEach(input => {
-            localStorage.removeItem(input.id);
-            input.value = '';
-        });
     });
 });
